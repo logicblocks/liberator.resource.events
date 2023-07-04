121060b5c17893ec0f397cf03ae01cd51eb27f57
Circle CI <circleci@logicblocks.io>
2023-07-04 21:30:30 +0000

    Pre-release version 0.1.0-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/liberator.resource.events "0.1.0-RC0"
+(defproject io.logicblocks/liberator.resource.events "0.1.0-RC1"
   :description "Event related resources for liberator."
   :url "https://github.com/logicblocks/liberator.resource.events"
 
